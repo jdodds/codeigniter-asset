3d4db3b658c03b4a78595bd3677db0c35144d48e
philsturgeon <email@philsturgeon.co.uk>
2009-09-15 04:46:16 -0700

    Made the default config a bit less weird.

--- config/asset.php
+++ config/asset.php
@@ -14,7 +14,7 @@
 
 // This crazy line means it will use the app path whether its in the web root or not
 // Feel free to change this to something more normal lookin!
-$config['asset_dir'] = parse_url(config_item('base_url'), PHP_URL_PATH).APPPATH;
+$config['asset_dir'] = parse_url(config_item('base_url'), PHP_URL_PATH).'/';
 
 /*
 |--------------------------------------------------------------------------
@@ -24,13 +24,12 @@ $config['asset_dir'] = parse_url(config_item('base_url'), PHP_URL_PATH).APPPATH;
 | URL to your CodeIgniter root. Typically this will be your base URL,
 | WITH a trailing slash:
 |
-|	/assets/
+|	http://example.com/assets/
 |
 */
 
 // Right now its pointing to the same place as the base_url.
-// You can move this to anywhere if you like
-$config['asset_url'] = config_item('base_url');
+$config['asset_url'] = config_item('base_url').'assets/';
 
 /*
 |--------------------------------------------------------------------------
@@ -39,7 +38,9 @@ $config['asset_url'] = config_item('base_url');
 |
 | Names for the img, js and css folders. Can be renamed to anything
 |
-|	/assets/
+| asset_img_dir = 'img';
+| asset_js_dir = 'js';
+| asset_css_dir = 'css';
 |
 */
 $config['asset_img_dir'] = 'img';
